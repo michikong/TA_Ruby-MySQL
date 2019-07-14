b802fe77bfbbfedf70536b48623b285b8fe1ae62
michinobu.sekikawa <abc_0615@icloud.com>
2019-07-15 08:05:06 +0900

    eachを埋め込む

--- test.rb
+++ test.rb
@@ -11,16 +11,11 @@ def web_page
     <title>タイトル</title>
   </head>
   <body>
-    <% name = '太郎' %>
-    <% time_japan = Time.now.getlocal("+09:00") %>
-    <p>今は<%= time_japan.hour %>時です。</p>
-    <% if 6 <= time_japan.hour && time_japan.hour < 12 %>
-      <p>おはよう、<%= name %>さん</p>
-    <% elsif 12 <= time_japan.hour && time_japan.hour < 18 %>
-      <p>こんにちは、<%= name %>さん</p>
-    <% else %>
-      <p>こんばんは、<%= name %>さん</p>
-    <% end %>
+    <ul>
+      <% (1..9).each do |num| %>
+        <li><%= num %></li>
+      <% end %>
+    </ul>
   </body>
 </html>
   PAGE
