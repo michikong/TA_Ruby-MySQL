ffcf910db97751be6523d2fa88b42e865646f09c
michinobu.sekikawa <abc_0615@icloud.com>
2019-07-15 08:00:18 +0900

    if文を埋め込む

--- test.rb
+++ test.rb
@@ -11,7 +11,16 @@ def web_page
     <title>タイトル</title>
   </head>
   <body>
-    <p><%= Time.now %></p>
+    <% name = '太郎' %>
+    <% time_japan = Time.now.getlocal("+09:00") %>
+    <p>今は<%= time_japan.hour %>時です。</p>
+    <% if 6 <= time_japan.hour && time_japan.hour < 12 %>
+      <p>おはよう、<%= name %>さん</p>
+    <% elsif 12 <= time_japan.hour && time_japan.hour < 18 %>
+      <p>こんにちは、<%= name %>さん</p>
+    <% else %>
+      <p>こんばんは、<%= name %>さん</p>
+    <% end %>
   </body>
 </html>
   PAGE
