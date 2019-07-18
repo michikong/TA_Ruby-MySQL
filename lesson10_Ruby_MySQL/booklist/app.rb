a164b730866190ea52abbbfef801cca13e408256
michinobu.sekikawa <abc_0615@icloud.com>
2019-07-19 07:27:55 +0900

    6.2 通信した変数からSQLを実行する

--- lesson10_Ruby_MySQL/booklist/app.rb
+++ lesson10_Ruby_MySQL/booklist/app.rb
@@ -3,6 +3,7 @@ require 'mysql2'
 
 get '/' do
   client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'booklist', encoding: 'utf8')
+  @records = client.query("SELECT * FROM books ORDER BY created_at DESC")
   erb :booklist
 end
 
